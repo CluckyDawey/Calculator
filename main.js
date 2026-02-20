665e5216b0f5afe93ca14e438f2a60bc76672232
Andre Bundal <andrecarlosbundal@gmail.com>
2026-02-20 15:16:55 +0800

    Removed the dot button for decimal and replaced it with a clear button

---
+++ main.js
@@ -0,0 +1,23 @@
+function sum(num1,num2) {
+    return num1 + num2
+}
+
+function subtract(num1,num2) {
+    return num1 - num2
+}
+
+function multiply(num1,num2) {
+    return num1 * num2
+}
+
+function divide(num1,num2) {
+    return num1 / num2
+}
+
+let firstNum = 0;
+let secondNUm = 0;
+let selectedOperator = "";
+
+function operate(operator,num1,num2) {
+    
+}
