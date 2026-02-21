77a62f078292da9f53aaf922a070033b6500b4b4
Andre Bundal <andrecarlosbundal@gmail.com>
2026-02-21 15:55:06 +0800

    Did the operate function(a compilation of the basic operator functions)
    added eventlisteners for the operator buttons
    
    added a little variable reset in clearNums eventlisteners

--- main.js
+++ main.js
@@ -14,17 +14,34 @@ function divide(num1,num2) {
     return num1 / num2
 }
 
-let firstNum = 0;
-let secondNUm = 0;
+let total = 0;
+let tempNum = 0;
 let selectedOperator = "";
 
 function operate(operator,num1,num2) {
-
+    if (operator == "+") return sum(num1,num2);
+    if (operator == "-") return subtract(num1,num2);
+    if (operator == "*") return multiply(num1,num2);
+    if (operator == "/") return divide(num1,num2);
 }
 
 let display = document.getElementById("display-screen");
-const numbersPad = document.querySelectorAll(".nmbr")
-const clearNums = document.getElementById("clear")
-clearNums.addEventListener("click", () => display.textContent = "")
+const numbersPad = document.querySelectorAll(".nmbr");
+const clearNums = document.getElementById("clear");
+const operatorAdd = document.getElementById("plus");
+const operatorMinus = document.getElementById("minus");
+const operatorMultiply = document.getElementById("multiply");
+const operatorDivide = document.getElementById("divide");
+const equalOperator = document.getElementById("equal");
 
-numbersPad.forEach(pad => pad.addEventListener("click", () => display.textContent += pad.textContent))
+
+
+clearNums.addEventListener("click", () => {
+    display.textContent = "";
+    total = 0;
+    tempNum = 0;
+})
+
+numbersPad.forEach(pad => pad.addEventListener("click", () => {
+    display.textContent += pad.textContent
+}))
